0bd377a5fbdb17db991364bcd7efb466edb77826
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-02 00:40:28 +0900

    added association via models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :pictures
+  has_many :favorites, dependent: :destroy
+
+  
+  
+  has_secure_password
+  mount_uploader :image, ImageUploader
+  
+end
+
 end
