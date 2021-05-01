9913434739f63840d3453462cfbd99621b08414c
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-02 00:42:01 +0900

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
 
-  
+  validates :name, presence: true, length: { maximum:30 }
+  validates :email, presence: true:, length { maximum:255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+  validates :password, length: {minimum: 6}
   
   has_secure_password
   mount_uploader :image, ImageUploader
