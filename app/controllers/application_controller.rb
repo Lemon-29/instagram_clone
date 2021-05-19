63989e20d371f942bd086f217ed9c328940ff6da
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-19 14:01:10 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :login_required
   include SessionsHelper
   def authenticate_user
     if @current_user == nil
